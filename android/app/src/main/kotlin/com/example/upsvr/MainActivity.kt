1769610696ea4fdd4105adadc02105ca1f1dcf18
Thai Nguyen Xuan <nxthai@outlook.com>
2022-07-23 23:20:54 +0700

    dd

--- android/app/src/main/kotlin/com/example/upsvr/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/upsvr/MainActivity.kt
@@ -3,5 +3,21 @@ package com.example.upsvr
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    
+https://review.gerrithub.io/c/thainxo/upsvr/+/541530 
+{
+    "verified" : "-1",
+    "message" : "Test",
+    "comments" : {
+        "android\app\src\main\kotlin\com\example\upsvr\MainActivity.kt" : [
+            {
+                "line": 12,
+                message: "test ccpp"
+            }
+        ]
+    }
+}
+
+cat test.json | ssh thainxo@review.gerrithub.io -p 29418 gerrit review 541530,1 --json --label Verified=-1 --project thainxo/upsvr 
+ssh thainxo@review.gerrithub.io -p 29418 gerrit query
+  ssh thainxo@review.gerrithub.io -p 29418 gerrit query --comments change:541530,1 --format JSON
 }
